42d2e20f8771e196b7974cbd146c1d4e18b5b3ad
Vivek Shaw <M1044439@mindtree.com>
2018-04-19 08:28:38 +0530

    Added implementation for Repository methods.

--- MyNiceHome.BusinessManager/UserUtility.cs
+++ MyNiceHome.BusinessManager/UserUtility.cs
@@ -9,24 +9,49 @@ using MyNiceHome.Entities;
 
 namespace MyNiceHome.BusinessManager
 {
+    /// <summary>
+    /// UserUtility Class representing User Utilities
+    /// </summary>
     public class UserUtility : IUserUtility
     {
+        /// <summary>
+        /// ReadOnly Reference for IRepositoryUtility Interface
+        /// </summary>
         private readonly IRepositoryUtility _repositoryUtility;
+
+        /// <summary>
+        /// UserUtility Constructor
+        /// </summary>
+        /// <param name="repositoryUtility"></param>
         public UserUtility(IRepositoryUtility repositoryUtility)
         {
             _repositoryUtility = repositoryUtility;
         }
 
+        /// <summary>
+        /// Sender Method for Adding Host Details
+        /// </summary>
+        /// <param name="host"></param>
+        /// <returns></returns>
         public bool CreateNewHost(Host host)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Sender Method for Adding Traveller Details
+        /// </summary>
+        /// <param name="traveller"></param>
+        /// <returns></returns>
         public bool CreateNewTraveller(Traveller traveller)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Method for Creating New Connection
+        /// </summary>
+        /// <returns></returns>
         public bool GetConnection()
         {
             _repositoryUtility.GetNewConnection();

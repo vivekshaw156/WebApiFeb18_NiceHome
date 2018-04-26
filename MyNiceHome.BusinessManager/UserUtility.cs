a59f56c0d3f1f2ebb2b311a18e95e5b2e3172969
Arkadeep Saha (IN4398) <M1044398@mindtree.com>
2018-04-26 12:42:44 +0530

    Changed backend code to reflect email changes in frontend

--- MyNiceHome.BusinessManager/UserUtility.cs
+++ MyNiceHome.BusinessManager/UserUtility.cs
@@ -1,16 +1,11 @@
 ﻿using MyNiceHome.BusinessManager.Interfaces;
 using MyNiceHome.Repository;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using MyNiceHome.Entities;
 using System.Text.RegularExpressions;
 using MyNiceHome.Exceptions;
 using MyNiceHome.Manager.Helpers;
-using System.IdentityModel.Tokens.Jwt;
-using Microsoft.IdentityModel.Tokens;
 using MyNiceHome.BusinessManager.MyNiceHome.BusinessManager.Helpers;
 
 namespace MyNiceHome.BusinessManager
@@ -50,34 +45,42 @@ namespace MyNiceHome.BusinessManager
             var cityHost = (host.HostCity).ToUpper();
             var patternName = new Regex(@"^[a-zA-Z][a-z A-Z\s-]+$");
             var patternCity = new Regex(@"^[A-Z]+$");
-            var patternEmail = new Regex(@"^[a-zA-Z0-9](\.?[a-zA-Z0-9]){1,}@([a-zA-Z]+)\.com$");
+            //var patternEmail = new Regex(@"^[a-zA-Z0-9](\.?[a-zA-Z0-9]){1,}@([a-zA-Z]+)\.com$");
             var patternPhone = new Regex(@"^[0-9]*$");
 
             if (host == null)
             {
                 throw new NullReferenceException("host");
             }
-            else if (host.HostName == null || !(patternName.Match(host.HostName).Success))
+            if (host.HostName == null || !(patternName.Match(host.HostName).Success))
             {
                 throw new InvalidNameException("Please enter a valid name");
             }
-            else if (host.HostCity == null || !(patternCity.Match(cityHost).Success))
+            if (host.HostCity == null || !(patternCity.Match(cityHost).Success))
             {
                 throw new InvalidCityException("Please enter a valid city");
             }
-            else if (host.HostEmail == null || !(patternEmail.Match(host.HostEmail).Success))
+            if (host.HostEmail == null)
             {
                 throw new InvalidEmailException("Please enter a valid email");
             }
-            else if (host.HostPhone == null || host.HostPhone.Length != 10 || !(patternPhone.Match(host.HostPhone).Success))
+            try
+            {
+                var address = new System.Net.Mail.MailAddress(host.HostEmail);
+            }
+            catch
+            {
+                new InvalidEmailException("Please enter a valid email");
+            }
+            if (host.HostPhone == null || host.HostPhone.Length != 10 || !(patternPhone.Match(host.HostPhone).Success))
             {
                 throw new InvalidPhoneNumberException("Please enter a valid phone number");
             }
-            else if (host.HostPassword == null || host.HostPassword.Length < 8)
+            if (host.HostPassword == null || host.HostPassword.Length < 8)
             {
                 throw new InvalidPasswordException("Please enter a valid password");
             }
-            else if (await _repositoryUtility.CheckIfHostExists(host))
+            if (await _repositoryUtility.CheckIfHostExists(host))
             {
                 throw new DuplicateEntryException("Host already exists");
             }
@@ -110,38 +113,47 @@ namespace MyNiceHome.BusinessManager
 
             var patternName = new Regex(@"^[a-zA-Z][a-z A-Z\s-]+$");
             var patternCity = new Regex(@"^[A-Z]+$");
-            var patternEmail = new Regex(@"^[a-zA-Z0-9](\.?[a-zA-Z0-9]){1,}@([a-zA-Z]+)\.com$");
+            //var patternEmail = new Regex(@"^[a-zA-Z0-9](\.?[a-zA-Z0-9]){1,}@([a-zA-Z]+)\.com$");
             var patternPhone = new Regex(@"^[0-9]*$");
 
             if (traveller == null)
             {
                 throw new ArgumentNullException("traveller");
             }
-            else if (traveller.TravellerName == null || !(patternName.Match(traveller.TravellerName).Success))
+            if (traveller.TravellerName == null || !(patternName.Match(traveller.TravellerName).Success))
             {
                 throw new InvalidNameException("Please enter a valid name");
             }
             //checking invalid entry for city field
-            else if (traveller.TravellerCity == null || !(patternCity.Match(traveller.TravellerCity).Success))
+            if (traveller.TravellerCity == null || !(patternCity.Match(traveller.TravellerCity).Success))
             {
                 throw new InvalidCityException("Please enter a valid city");
             }
             //checking invalid entry for email field
-            else if (traveller.TravellerEmail == null || !(patternEmail.Match(traveller.TravellerEmail).Success))
+            if (traveller.TravellerEmail == null)
             {
                 throw new InvalidEmailException("Please enter a valid email");
             }
+            try
+            {
+                var address = new System.Net.Mail.MailAddress(traveller.TravellerEmail);
+            }
+            catch
+            {
+                throw new InvalidEmailException("Please enter a valid email");
+            }
+
             //checking invalid entry for phone number field
-            else if (traveller.TravellerPhone == null || traveller.TravellerPhone.Length != 10 || !(patternPhone.Match(traveller.TravellerPhone).Success))
+            if (traveller.TravellerPhone == null || traveller.TravellerPhone.Length != 10 || !(patternPhone.Match(traveller.TravellerPhone).Success))
             {
                 throw new InvalidPhoneNumberException("Please enter a valid phone number");
             }
             //checking invalid entry for password field
-            else if (traveller.TravellerPassword == null || traveller.TravellerPassword.Length < 8)
+            if (traveller.TravellerPassword == null || traveller.TravellerPassword.Length < 8)
             {
                 throw new InvalidPasswordException("Please enter a valid password");
             }
-            else if (await _repositoryUtility.CheckIfTravellerExists(traveller))
+            if (await _repositoryUtility.CheckIfTravellerExists(traveller))
             {
                 throw new DuplicateEntryException("Traveller already exists");
             }

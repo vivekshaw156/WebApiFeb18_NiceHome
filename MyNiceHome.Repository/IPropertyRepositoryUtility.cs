53f743b7f45fd4dac6314a1719c6b30845ff487a
Vivek Shaw <M1044439@mindtree.com>
2018-04-29 12:08:09 +0530

    Added search property methods in all layers.

---
+++ MyNiceHome.Repository/IPropertyRepositoryUtility.cs
@@ -0,0 +1,15 @@
+﻿using MyNiceHome.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNiceHome.Repository
+{
+    public interface IPropertyRepositoryUtility
+    {
+        List<Property> GetPropertyDetails(string place, string fromDate, string toDate, int people);
+
+    }
+}
